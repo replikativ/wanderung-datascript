cf843c105a2dcd80c48bacd44a06f445006bd7a8
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2021-10-18 17:11:17 +0200

    add simple schema test

--- src/wanderung/datascript.clj
+++ src/wanderung/datascript.clj
@@ -1,5 +1,27 @@
 (ns wanderung.datascript
-  
-  )
+  (:require
+   [wanderung.core :as w :refer [create-source]]
+   [datascript.core :as ds])
+  (:import
+   [wanderung.core IConnection IExtract]))
 
+(defn resolve-schema [db]
+  (->> db
+       :schema
+       (mapv (fn [[ident schema-def]]
+               (assoc schema-def :db/ident ident)))))
 
+(defrecord DataScriptDB [state db]
+  IConnection
+  (connect [{:keys [state db]}]
+    (swap! state assoc :db db))
+  IExtract
+  (extract-datoms [{:keys [state]}]
+    (->> (ds/datoms (:db @state) :eavt)
+         (mapv (fn [d] (vec (seq d))))
+         (sort-by #(nth % 3))
+         vec)))
+
+(defmethod create-source :datascript [{:keys [db]}]
+  (map->DataScriptDB {:state (atom nil)
+                      :db db}))
--- test/wanderung/datascript_test.clj
+++ test/wanderung/datascript_test.clj
@@ -1,7 +1,74 @@
 (ns wanderung.datascript-test
   (:require [clojure.test :refer :all]
-            [wanderung.datascript :refer :all]))
+            [datascript.core :as ds]
+            [datahike.api :as dh]
+            [wanderung.core :refer [migrate]]
+            [wanderung.datascript :refer [resolve-schema]]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+(deftest test-simple-import
+  (testing "simple data and no schema"
+    (let [
+          {datascript-db :db-after} (ds/with
+                                     (ds/empty-db)
+                                     [{:db/id 1
+                                       :name  "Alice"
+                                       :age   35}
+                                      {:db/id 2
+                                       :name  "Bob"
+                                       :age   40}])
+          source-cfg                {:db             datascript-db
+                                     :wanderung/type :datascript}
+          target-cfg                {:store           {:backend :mem
+                                                       :id      "data-target"}
+                                     :attribute-refs? false
+                                     :keep-history?   false
+                                     :wanderung/type  :datahike
+                                     :name            "Data Target"}
+          _                         (do
+                                      (dh/delete-database target-cfg)
+                                      (dh/create-database target-cfg))]
+      (migrate source-cfg target-cfg)
+      (let [dh-conn (dh/connect target-cfg)
+            query '[:find ?n ?a
+                    :where
+                    [?e :name ?n]
+                    [?e :age ?a]]]
+        (is (= (ds/q query datascript-db)
+               (dh/q query @dh-conn))))))
+  (testing "simple data and schema"
+    (let [datascript-schema         {:parents {:db/valueType   :db.type/ref
+                                               :db/cardinality :db.cardinality/many}}
+          {datascript-db :db-after} (ds/with
+                                     (ds/empty-db datascript-schema)
+                                     [{:db/id 1
+                                       :name  "Alice"
+                                       :age   35}
+                                      {:db/id 2
+                                       :name  "Bob"
+                                       :age   40}
+                                      {:db/id   3
+                                       :name    "Charlie"
+                                       :age     5
+                                       :parents [1 2]}])
+          source-cfg                {:db             datascript-db
+                                     :wanderung/type :datascript}
+          target-cfg                {:store           {:backend :mem
+                                                       :id      "data-target"}
+                                     :attribute-refs? false
+                                     :keep-history?   false
+                                     :wanderung/type  :datahike
+                                     :initial-tx      (resolve-schema datascript-db)
+                                     :name            "Data Target"}
+          _                         (do
+                                      (dh/delete-database target-cfg)
+                                      (dh/create-database target-cfg))]
+      (migrate source-cfg target-cfg)
+      (let [dh-conn (dh/connect target-cfg)
+            query '[:find ?n ?a
+                    :where
+                    [?e :name ?n]
+                    [?e :age ?a]]]
+        (is (= (ds/q query datascript-db)
+               (dh/q query @dh-conn)))))))
